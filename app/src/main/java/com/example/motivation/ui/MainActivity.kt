8f9b7f4b878f9ef2c58556d8587c1bdb943f005c
João Paulo Faustino Santos <jpaulofsantos@gmail.com>
2023-02-08 21:41:39 -0300

    changing icons color

--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.motivation.R
 import com.example.motivation.databinding.ActivityMainBinding
 import com.example.motivation.utils.MotivationConstants
@@ -19,15 +20,34 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         supportActionBar?.hide()
 
+        binding.btnFrase.setOnClickListener(this)
         binding.ivLeft.setOnClickListener(this)
+        binding.ivCenter.setOnClickListener(this)
+        binding.ivRight.setOnClickListener(this)
 
         //buscando o nome de UserActivity e setando o nome no textView através da chave "nomeLogin"
         binding.tvNome.text = SharedInformation(this).getName(MotivationConstants.KEY.USER_NAME) + "!"
     }
 
     override fun onClick(view: View) {
-        if (view.id == R.id.ivLeft) {
-            R.drawable.ic_baseline_self_improvement_24
+        if (view.id in listOf(R.id.ivLeft, R.id.ivCenter, R.id.ivRight)) {
+            handleFilter(view.id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        binding.ivLeft.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.ivCenter.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.ivRight.setColorFilter(ContextCompat.getColor(this, R.color.black))
+
+        if (id == R.id.ivLeft) {
+            binding.ivLeft.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        } else if (id == R.id.ivCenter) {
+            binding.ivCenter.setColorFilter(ContextCompat.getColor(this, R.color.white))
+
+        } else if (id == R.id.ivRight) {
+            binding.ivRight.setColorFilter(ContextCompat.getColor(this, R.color.white))
         }
     }
 }
